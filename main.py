0ec98548ba618386be2a2c5e85eacb5fdb38554d
Hitesh Aggarwal <hitesh33231@gmail.com>
2023-04-18 19:48:36 +0530

    made the script user friendly

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+
 recom_books = []
 # Load the dataset
-books_df = pd.read_csv('DataSet.csv')
+books_df = pd.read_csv("data.csv")
 
 # Create a TF-IDF vectorizer to convert the summaries into a numerical representation
-tfidf = TfidfVectorizer(stop_words='english')
-books_df['summary'] = books_df['Summaries'].fillna('')
-tfidf_matrix = tfidf.fit_transform(books_df['summary'])
+tfidf = TfidfVectorizer(stop_words="english")
+books_df["summary"] = books_df["Summaries"].fillna("")
+tfidf_matrix = tfidf.fit_transform(books_df["summary"])
 
 # Calculate the cosine similarity between each book
 cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
+
 
 # Define a function to recommend similar books based on a given book
 def recommend_books(book_title, cosine_sim=cosine_sim, books_df=books_df):
     try:
         # Get the index of the book that matches the title
-        idx = books_df[books_df['Title'] == book_title].index[0]
+        idx = books_df[books_df["Title"] == book_title].index[0]
 
         # Get the similarity scores between the book and all the others
         sim_scores = list(enumerate(cosine_sim[idx]))
@@ -32,11 +34,11 @@ def recommend_books(book_title, cosine_sim=cosine_sim, books_df=books_df):
         book_indices = [i[0] for i in sim_scores]
 
         # Return the top 10 similar books
-        return books_df['Title'].iloc[book_indices]
+        return books_df["Title"].iloc[book_indices]
     except IndexError as e:
         print("Error:", e)
         return []
-    
+
 
 # Recommend books similar to any book required
 def predicted_books(book_name):
@@ -44,26 +46,16 @@ def predicted_books(book_name):
     recomm_books = recommend_books(book_name)
     for i in recomm_books:
         recom_books = recom_books + [i]
-    return (recom_books)
+    return recom_books
 
 
 def main():
-    #Take 1984 as an example
-    book_name = input('Previous Book:')
-    state = True
+    book_name = input("Enter Book Title: ")
     ans = predicted_books(book_name)
-    while(state):
-        print(ans[0])
-        del(ans[0])
-        # exit()
-        var = input('Recommend another book?(y/n)')
-        if(len(ans)>0):
-            if(var == 'y'):
-                print('New Book: ')
-            else:
-                state = False
-                print('Good Bye')
-        else:
-            print('No new Book Found(Out of Data)')
-            state = False
-main()
+    print("You may also like:")
+    while ans:
+        print(ans.pop())
+
+
+if __name__ == "__main__":
+    main()
